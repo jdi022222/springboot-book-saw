7b5f51f745bb916de5e1c34e874b6fc27dcb74ff
jdi022222 <jdi022222@gmail.com>
2023-06-12 11:04:48 +0900

    feat: 게시글 수정 화면 구현

--- src/main/java/com/comibird/web/IndexController.java
+++ src/main/java/com/comibird/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.comibird.web;
 
 import com.comibird.service.post.PostService;
+import com.comibird.web.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postSave() {
         return "posts-save";
+    }
+
+    @GetMapping("posts/update/{id}")
+    public String postUpdate(@PathVariable("id") Long id, Model model) {
+        PostResponseDto dto = postService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
 
