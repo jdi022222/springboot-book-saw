538052ab50a11c4aed9a3ca8178e95c41aedf744
jdi022222 <jdi022222@gmail.com>
2023-06-13 12:17:11 +0900

    fix: 로그인 오류 수정

--- src/main/java/com/comibird/web/IndexController.java
+++ src/main/java/com/comibird/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.comibird.web;
 
+import com.comibird.config.auth.dto.SessionUser;
 import com.comibird.service.post.PostService;
 import com.comibird.web.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostService postService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
