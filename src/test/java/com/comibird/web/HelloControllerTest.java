b8300ccbaea1c63f0d276fae9f8ca333f2aea0e1
jdi022222 <jdi022222@gmail.com>
2023-06-15 01:16:45 +0900

    test: HelloController 테스트에 MockUser 추가, config 스캔 제외

--- src/test/java/com/comibird/web/HelloControllerTest.java
+++ src/test/java/com/comibird/web/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.comibird.web;
 
+import com.comibird.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -12,15 +16,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // JUnit에 내장된 실행자 대신 다른 실행자 실행
 // 스프링부트테스트와 JUnit 사이의 연결자
 // 여기선 controller만 사용
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     // 스프링이 관리하는 빈(bean)을 주입받음
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
-    public void hello가_return된다() throws Exception {
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -28,14 +36,16 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
+
         mvc.perform(
-                get("/hello/dto")
-                        .param("name", name)
-                        .param("amount", String.valueOf(amount)))
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
